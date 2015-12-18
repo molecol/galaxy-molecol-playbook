9f7704ab1ff5ad5a6e185af4fd82c610d6f9da5a
Michal Stuglik <stuglik@gmail.com>
2015-12-18 12:09:47 +0000

    refactored code - removed redundancy - configs for code version and use - conditionals for mode use

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-/usr/bin/env ansible-playbook ../deploy.yml -i ../inventory/staging "$@"
+# Example commands
+#   /usr/bin/env    ansible-playbook ../deploy.yml -i ../inventory/staging "$@"
+#   /usr/bin/env    ansible-playbook ../deploy.yml -i ../inventory/staging "$@" -e mode=production
+#   /usr/bin/env    ansible-playbook ../deploy.yml -i ../inventory/staging "$@" -e repo=gp-dev
+
+/usr/bin/env ansible-playbook ../deploy.yml -i .../inventory/staging "$@"
+
--- scripts/staging_provision.sh
+++ scripts/staging_provision.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+# Example commands
+#   /usr/bin/env    ansible-playbook ../provision.yml -i ../inventory/staging "$@" -K -e mode=production repo=release_15.10
+#   /usr/bin/env    ansible-playbook ../provision.yml -i ../inventory/staging "$@" -K -e mode=lite repo=release_15.10
+#   /usr/bin/env    ansible-playbook ../provision.yml -i ../inventory/staging "$@" -K -e mode=production repo=gp-dev
+
 /usr/bin/env ansible-playbook ../provision.yml -i ../inventory/staging "$@" -K
